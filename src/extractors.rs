5fe72030d74cad02299923462b82d978efcae28d
Tobias Messner <tobias.d.messner@gmail.com>
2023-02-21 16:40:27 +0100

    Use bb8 instead of actix_redis, add logging

---
+++ src/extractors.rs
@@ -0,0 +1,173 @@
+use actix_web::error::{ErrorBadRequest, ErrorInternalServerError};
+use num_traits::FromPrimitive;
+use scraper::Html;
+
+use crate::{Rank, Role, Tier, TierNumber};
+use url::Url;
+
+pub fn extract_profile_picture(document: &Html) -> actix_web::Result<Option<String>> {
+    let profile_picture_selector = scraper::Selector::parse(".Profile-player--portrait")
+        .map_err(|_| crate::parsing_error())?;
+
+    Ok(document
+        .select(&profile_picture_selector)
+        .next()
+        .ok_or_else(crate::parsing_error)?
+        .value()
+        .attr("src")
+        .map_or_else(
+            || None,
+            |s| {
+                if s.is_empty() {
+                    None
+                } else {
+                    Some(s.to_owned()) // Better semantics imo
+                }
+            },
+        ))
+}
+
+pub fn extract_title(document: &Html) -> actix_web::Result<Option<String>> {
+    let title_selector =
+        scraper::Selector::parse(".Profile-player--title").map_err(|_| crate::parsing_error())?;
+    let title_element = document.select(&title_selector).next();
+
+    let title_inner_text = title_element.map(|e| e.inner_html());
+    Ok(title_inner_text.map_or_else(
+        || None,
+        |s| {
+            if s.is_empty() || s == "No Title" {
+                None
+            } else {
+                Some(s)
+            }
+        },
+    ))
+}
+
+// Verwendest du mehrmals plus alternative mit and_then
+fn url_file_name(url: &Url) -> actix_web::Result<&str> {
+    url.path_segments()
+        .and_then(Iterator::last)
+        .ok_or_else(crate::parsing_error)
+}
+
+pub fn extract_endorsement(document: &Html) -> actix_web::Result<u8> {
+    let endorsement_selector = scraper::Selector::parse(".Profile-playerSummary--endorsement")
+        .map_err(|_| crate::parsing_error())?;
+
+    let endorsement_url = Url::parse(
+        document
+            .select(&endorsement_selector)
+            .next()
+            .ok_or_else(crate::parsing_error)?
+            .value()
+            .attr("src")
+            .ok_or_else(crate::parsing_error)?,
+    )
+    .map_err(|_| ErrorBadRequest("Could not parse endorsement URL"))?;
+
+    let endorsement = match url_file_name(&endorsement_url)? {
+        "1-9de6d43ec5.svg" => 1,
+        "2-8b9f0faa25.svg" => 2,
+        "3-8ccb5f0aef.svg" => 3,
+        "4-48261e1164.svg" => 4,
+        "5-8697f241ca.svg" => 5,
+        _ => {
+            return Err(ErrorInternalServerError(
+                "Found invalid endorsement level while parsing",
+            ))
+        }
+    };
+
+    Ok(endorsement)
+}
+
+pub fn extract_roles(
+    document: &Html,
+) -> actix_web::Result<(Option<Rank>, Option<Rank>, Option<Rank>)> {
+    let role_wrapper_selector = scraper::Selector::parse(".Profile-playerSummary--roleWrapper")
+        .map_err(|_| crate::parsing_error())?;
+
+    let role_selector = scraper::Selector::parse(".Profile-playerSummary--role>img")
+        .map_err(|_| crate::parsing_error())?;
+    let tier_selector = scraper::Selector::parse(".Profile-playerSummary--rank")
+        .map_err(|_| crate::parsing_error())?;
+
+    let mut tank: Option<Rank> = None;
+    let mut damage: Option<Rank> = None;
+    let mut support: Option<Rank> = None;
+
+    let rank_container = document.select(&role_wrapper_selector);
+
+    for rank in rank_container {
+        let role_url = Url::parse(
+            rank.select(&role_selector)
+                .next()
+                .ok_or_else(crate::parsing_error)?
+                .value()
+                .attr("src")
+                .ok_or_else(crate::parsing_error)?,
+        )
+        .map_err(|_| ErrorBadRequest("Could not parse Role URL"))?;
+        let tier_url = Url::parse(
+            rank.select(&tier_selector)
+                .next()
+                .ok_or_else(crate::parsing_error)?
+                .value()
+                .attr("src")
+                .ok_or_else(crate::parsing_error)?,
+        )
+        .map_err(|_| ErrorBadRequest("Could not parse Tier URL"))?;
+
+        let role = match url_file_name(&role_url)? {
+            "tank-f64702b684.svg" => Role::Tank,
+            "offense-ab1756f419.svg" => Role::Damage,
+            "support-0258e13d85.svg" => Role::Support,
+            _ => return Err(ErrorInternalServerError("Found invalid role while parsing")),
+        };
+
+        // splitn macht soweit ich weiß keinen Unterschied
+        let mut tier_parts = url_file_name(&tier_url)?.split('-');
+        let tier_name = match tier_parts.next().ok_or_else(crate::parsing_error)? {
+            "BronzeTier" => Tier::Bronze,
+            "SilverTier" => Tier::Silver,
+            "GoldTier" => Tier::Gold,
+            "PlatinumTier" => Tier::Platinum,
+            "DiamondTier" => Tier::Diamond,
+            "MasterTier" => Tier::Master,
+            "GrandmasterTier" => Tier::Grandmaster,
+            _ => return Err(ErrorInternalServerError("Found invalid tier while parsing")),
+        };
+
+        let tier_number: TierNumber = FromPrimitive::from_u8(
+            tier_parts
+                .next()
+                .ok_or_else(crate::parsing_error)?
+                .parse::<u8>()
+                .map_err(|_| ErrorInternalServerError("Found invalid tier number while parsing"))?,
+        )
+        .ok_or_else(crate::parsing_error)?;
+        match role {
+            Role::Tank => {
+                tank = Some(Rank {
+                    tier: tier_name,
+                    tier_number,
+                })
+            }
+            Role::Damage => {
+                damage = Some(Rank {
+                    tier: tier_name,
+                    tier_number,
+                })
+            }
+            Role::Support => {
+                support = Some(Rank {
+                    tier: tier_name,
+                    tier_number,
+                })
+            }
+        }
+    }
+    Ok((tank, damage, support))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,33 @@
-use actix::Addr;
 use actix_cors::Cors;
-use actix_redis::{resp_array, Command, RedisActor, RespValue};
 use actix_web::{
     error::{ErrorBadRequest, ErrorInternalServerError, ErrorNotFound},
     get, web, App, HttpResponse, HttpServer, Responder, Result,
 };
-use num::FromPrimitive;
-use num_derive::FromPrimitive;
-use reqwest::{StatusCode, Url};
+use bb8_redis::{bb8, redis::AsyncCommands, RedisConnectionManager};
+use reqwest::StatusCode;
 use scraper::Html;
+use std::env;
+use std::sync::Arc;
+use tokio::sync::Semaphore;
+
+use num_derive::FromPrimitive;
 use serde::{Deserialize, Serialize};
 use serde_repr::{Deserialize_repr, Serialize_repr};
-use std::sync::Arc;
-use std::{env, fmt::Display, time::Instant};
-use tokio::sync::Semaphore;
+use std::fmt::Display;
+
+use slog::{debug, error, info, o, Drain, Logger};
+
+mod extractors;
 
 struct AppState {
     client: reqwest::Client,
     semaphore: Arc<Semaphore>,
-    redis: Addr<RedisActor>,
+    redis_pool: bb8::Pool<RedisConnectionManager>,
+    log: Logger,
 }
 
 #[derive(Serialize, Deserialize)]
-struct Battletag {
+pub struct Battletag {
     name: String,
     discriminator: u32,
 }
@@ -62,7 +67,7 @@ enum TierNumber {
 }
 
 #[derive(Serialize, Deserialize, Clone)]
-struct Rank {
+pub struct Rank {
     tier: Tier,
     tier_number: TierNumber,
 }
@@ -80,178 +85,17 @@ struct Player {
     support: Option<Rank>,
 }
 
-// Preference since one |_| every time is acceptable
-// explicit error type since you might have multiple (also preference)
+fn configure_log() -> Logger {
+    let decorator = slog_term::TermDecorator::new().build();
+    let drain = slog_term::FullFormat::new(decorator).build().fuse();
+    let drain = slog_async::Async::new(drain).build().fuse();
+    Logger::root(drain, o!())
+}
+
 fn parsing_error() -> actix_web::Error {
     ErrorInternalServerError("Could not parse page")
 }
 
-fn extract_profile_picture(document: &Html) -> Result<Option<String>> {
-    let profile_picture_selector =
-        scraper::Selector::parse(".Profile-player--portrait").map_err(|_| parsing_error())?;
-
-    Ok(document
-        .select(&profile_picture_selector)
-        .next()
-        .ok_or_else(parsing_error)?
-        .value()
-        .attr("src")
-        .map_or_else(
-            || None,
-            |s| {
-                if s.is_empty() {
-                    None
-                } else {
-                    Some(s.to_owned()) // Better semantics imo
-                }
-            },
-        ))
-}
-
-fn extract_title(document: &Html) -> Result<Option<String>> {
-    let title_selector =
-        scraper::Selector::parse(".Profile-player--title").map_err(|_| parsing_error())?;
-    let title_element = document.select(&title_selector).next();
-
-    let title_inner_text = title_element.map(|e| e.inner_html());
-    Ok(title_inner_text.map_or_else(
-        || None,
-        |s| {
-            if s.is_empty() || s == "No Title" {
-                None
-            } else {
-                Some(s)
-            }
-        },
-    ))
-}
-
-// Verwendest du mehrmals plus alternative mit and_then
-fn url_file_name(url: &Url) -> Result<&str> {
-    url.path_segments()
-        .and_then(Iterator::last)
-        .ok_or_else(parsing_error)
-}
-
-fn extract_endorsement(document: &Html) -> Result<u8> {
-    let endorsement_selector = scraper::Selector::parse(".Profile-playerSummary--endorsement")
-        .map_err(|_| parsing_error())?;
-
-    let endorsement_url = Url::parse(
-        document
-            .select(&endorsement_selector)
-            .next()
-            .ok_or_else(parsing_error)?
-            .value()
-            .attr("src")
-            .ok_or_else(parsing_error)?,
-    )
-    .map_err(|_| ErrorBadRequest("Could not parse endorsement URL"))?;
-
-    let endorsement = match url_file_name(&endorsement_url)? {
-        "1-9de6d43ec5.svg" => 1,
-        "2-8b9f0faa25.svg" => 2,
-        "3-8ccb5f0aef.svg" => 3,
-        "4-48261e1164.svg" => 4,
-        "5-8697f241ca.svg" => 5,
-        _ => {
-            return Err(ErrorInternalServerError(
-                "Found invalid endorsement level while parsing",
-            ))
-        }
-    };
-
-    Ok(endorsement)
-}
-
-fn extract_roles(document: &Html) -> Result<(Option<Rank>, Option<Rank>, Option<Rank>)> {
-    let role_wrapper_selector = scraper::Selector::parse(".Profile-playerSummary--roleWrapper")
-        .map_err(|_| parsing_error())?;
-
-    let role_selector = scraper::Selector::parse(".Profile-playerSummary--role>img")
-        .map_err(|_| parsing_error())?;
-    let tier_selector =
-        scraper::Selector::parse(".Profile-playerSummary--rank").map_err(|_| parsing_error())?;
-
-    let mut tank: Option<Rank> = None;
-    let mut damage: Option<Rank> = None;
-    let mut support: Option<Rank> = None;
-
-    let rank_container = document.select(&role_wrapper_selector);
-
-    for rank in rank_container {
-        let role_url = Url::parse(
-            rank.select(&role_selector)
-                .next()
-                .ok_or_else(parsing_error)?
-                .value()
-                .attr("src")
-                .ok_or_else(parsing_error)?,
-        )
-        .map_err(|_| ErrorBadRequest("Could not parse Role URL"))?;
-        let tier_url = Url::parse(
-            rank.select(&tier_selector)
-                .next()
-                .ok_or_else(parsing_error)?
-                .value()
-                .attr("src")
-                .ok_or_else(parsing_error)?,
-        )
-        .map_err(|_| ErrorBadRequest("Could not parse Tier URL"))?;
-
-        let role = match url_file_name(&role_url)? {
-            "tank-f64702b684.svg" => Role::Tank,
-            "offense-ab1756f419.svg" => Role::Damage,
-            "support-0258e13d85.svg" => Role::Support,
-            _ => return Err(ErrorInternalServerError("Found invalid role while parsing")),
-        };
-
-        // splitn macht soweit ich weiß keinen Unterschied
-        let mut tier_parts = url_file_name(&tier_url)?.split('-');
-        let tier_name = match tier_parts.next().ok_or_else(parsing_error)? {
-            "BronzeTier" => Tier::Bronze,
-            "SilverTier" => Tier::Silver,
-            "GoldTier" => Tier::Gold,
-            "PlatinumTier" => Tier::Platinum,
-            "DiamondTier" => Tier::Diamond,
-            "MasterTier" => Tier::Master,
-            "GrandmasterTier" => Tier::Grandmaster,
-            _ => return Err(ErrorInternalServerError("Found invalid tier while parsing")),
-        };
-
-        let tier_number: TierNumber = FromPrimitive::from_u8(
-            tier_parts
-                .next()
-                .ok_or_else(parsing_error)?
-                .parse::<u8>()
-                .map_err(|_| ErrorInternalServerError("Found invalid tier number while parsing"))?,
-        )
-        .ok_or_else(parsing_error)?;
-        match role {
-            Role::Tank => {
-                tank = Some(Rank {
-                    tier: tier_name,
-                    tier_number,
-                })
-            }
-            Role::Damage => {
-                damage = Some(Rank {
-                    tier: tier_name,
-                    tier_number,
-                })
-            }
-            Role::Support => {
-                support = Some(Rank {
-                    tier: tier_name,
-                    tier_number,
-                })
-            }
-        }
-    }
-    Ok((tank, damage, support))
-}
-
-/// extract path info using serde
 #[get("/player/{name}-{discriminator}")]
 async fn get_battletag(
     info: web::Path<Battletag>,
@@ -259,15 +103,39 @@ async fn get_battletag(
 ) -> Result<impl Responder> {
     let info = info.into_inner();
 
-    let redis_command = resp_array!["GET", format!("{}-{}", info.name, info.discriminator)];
-    let redis_result = data.redis.send(Command(redis_command)).await;
+    let request_log = data
+        .log
+        .new(o!("battletag" => format!("{}-{}", info.name.clone(), info.discriminator) ));
 
-    if let Ok(Ok(RespValue::BulkString(data_stream))) = redis_result {
-        return Ok(web::Json(serde_json::from_slice::<Player>(&data_stream)?));
+    info!(request_log, "Getting player");
+
+    debug!(request_log, "Making redis connection");
+    let mut redis_connection = data.redis_pool.get().await.map_err(|e| {
+        error!(request_log, "Could not get redis connection, {}", e);
+        ErrorInternalServerError("Could not get redis connection, this should never happen :(")
+    })?;
+
+    debug!(request_log, "Checking redis cache");
+    let redis_result: redis::RedisResult<String> = redis_connection
+        .get(format!("{}-{}", info.name, info.discriminator))
+        .await;
+
+    if let Ok(player) = redis_result {
+        debug!(request_log, "Hit redis cache, decoding...");
+        let player: Player = serde_json::from_str(&player).map_err(|_| parsing_error())?;
+
+        debug!(request_log, "Returning cached player");
+        return Ok(HttpResponse::Ok()
+            .insert_header(("X-Cache", "HIT"))
+            .json(player));
     }
-    let permit = data.semaphore.acquire().await.unwrap();
 
-    let start = Instant::now();
+    let permit = data.semaphore.acquire().await.map_err(|e| {
+        error!(request_log, "Could not acquire semaphore, {}", e);
+        ErrorInternalServerError("Could not acquire semaphore, this should never happen :(")
+    })?;
+
+    debug!(request_log, "Making request to Blizzard server");
     let res = data
         .client
         .get(format!(
@@ -276,11 +144,13 @@ async fn get_battletag(
         ))
         .send()
         .await
-        .map_err(ErrorInternalServerError)?; // ErrorInternalServerError nimmt beliebigen error type
+        .map_err(ErrorInternalServerError)?;
+    debug!(
+        request_log,
+        "Finished request to Blizzard server, dropping Semaphore permit"
+    );
 
     drop(permit);
-
-    println!("Request took {:?}", start.elapsed());
 
     match res.status() {
         StatusCode::NOT_FOUND => Err(ErrorNotFound("Player not found")),
@@ -288,13 +158,13 @@ async fn get_battletag(
         _ => Ok(()),
     }?;
 
+    debug!(request_log, "Reading response body");
     let body = res
         .text()
         .await
         .map_err(|_| ErrorBadRequest("Could not parse body"))?;
 
-    println!("Body took {:?}", start.elapsed());
-
+    debug!(request_log, "Parsing response body");
     let document = Html::parse_document(&body);
 
     let private_profile_selector =
@@ -302,12 +172,14 @@ async fn get_battletag(
 
     let private_profile = document.select(&private_profile_selector).next().is_some();
 
-    let profile_picture = extract_profile_picture(&document)?;
-    let title = extract_title(&document)?;
-    let endorsement = extract_endorsement(&document)?;
-    let (tank, damage, support) = extract_roles(&document)?;
+    debug!(request_log, "Extracting data from page");
 
-    println!("Parsing took {:?}", start.elapsed());
+    let profile_picture = extractors::extract_profile_picture(&document)?;
+    let title = extractors::extract_title(&document)?;
+    let endorsement = extractors::extract_endorsement(&document)?;
+    let (tank, damage, support) = extractors::extract_roles(&document)?;
+
+    debug!(request_log, "Done extracting data from page");
 
     let battletag = Battletag {
         name: info.name.clone(),
@@ -315,7 +187,7 @@ async fn get_battletag(
     };
 
     let player = Player {
-        name: battletag.to_string(), // Display trait von Battletag
+        name: battletag.to_string(),
         battletag,
         private: private_profile,
         profile_picture,
@@ -326,24 +198,30 @@ async fn get_battletag(
         support,
     };
 
+    debug!(request_log, "Constructing response");
+
     let player_json = serde_json::to_string(&player);
 
     if let Ok(player_json) = player_json {
-        let redis_command = resp_array![
-            "SET",
-            format!("{}-{}", info.name, info.discriminator),
-            player_json,
-            "EX",
-            "600"
-        ];
-        let redis_result = data.redis.send(Command(redis_command)).await;
+        debug!(request_log, "Caching player in redis");
+        let redis_result: redis::RedisResult<String> = redis_connection
+            .set_ex(
+                format!("{}-{}", info.name, info.discriminator),
+                player_json,
+                600,
+            )
+            .await;
 
         if let Err(e) = redis_result {
-            println!("Redis error: {:?}", e);
+            error!(request_log, "Redis error: {:?}", e);
         }
     }
 
-    Ok(web::Json(player))
+    info!(request_log, "Responding with player");
+
+    Ok(HttpResponse::Ok()
+        .insert_header(("X-Cache", "MISS"))
+        .json(player))
 }
 
 #[get("/")]
@@ -353,7 +231,20 @@ async fn index() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let log = configure_log();
+
+    let redis_url = env::var("REDIS_URL").unwrap_or_else(|_| "redis://127.0.0.1:6379".to_string());
+    info!(log, "Using redis url: {}", redis_url);
+
+    let manager = RedisConnectionManager::new(redis_url).expect("Could not create redis manager");
+    let pool = bb8::Pool::builder()
+        .build(manager)
+        .await
+        .expect("Could not create redis pool");
+
+    info!(log, "Starting server on port 8080");
+
+    HttpServer::new(move || {
         // In closure to avoid clone
         let client = reqwest::ClientBuilder::new()
             .user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36")
@@ -362,14 +253,9 @@ async fn main() -> std::io::Result<()> {
 
         let semaphore = Arc::new(Semaphore::new(20));
 
-        let redis_url = env::var("REDIS_URL").unwrap_or_else(|_| "127.0.0.1:6379".to_string());
-
-        let redis = RedisActor::start(redis_url);
-
-
         App::new()
             .wrap(Cors::permissive())
-            .app_data(web::Data::new(AppState { client, semaphore, redis }))
+            .app_data(web::Data::new(AppState { client, semaphore, redis_pool: pool.clone(), log: log.clone()}))
             .service(index)
             .service(get_battletag)
     })
